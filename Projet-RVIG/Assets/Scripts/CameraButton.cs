9accf9220ba71b6d86e8f4aa09bc7e5969d461cc
Raton-mng <mathis.finet9@gmail.com>
2025-02-28 10:57:51 +0100

    On peut avoir plusieurs cam avec des switch !

---
+++ Projet-RVIG/Assets/Scripts/CameraButton.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class CameraButton : MonoBehaviour
+{
+    public Camera associatedCamera;
+    [SerializeField] private Button button;
+    private UnityEvent<Camera> _activateMyCamera;
+    
+    private void Start()
+    {
+        button.onClick.AddListener(Activate);
+        _activateMyCamera = new UnityEvent<Camera>();
+        _activateMyCamera.AddListener(SwitchCamera.Instance.ChangeCamera);
+    }
+
+    private void Activate()
+    {
+        _activateMyCamera.Invoke(associatedCamera);
+    }
+}
--- Projet-RVIG/Assets/Scripts/SwitchCamera.cs
+++ Projet-RVIG/Assets/Scripts/SwitchCamera.cs
@@ -6,20 +6,39 @@ using UnityEngine.UI;
 
 public class SwitchCamera : MonoBehaviour
 {
-    [SerializeField] private Button button;
+    /*[SerializeField] private Button button;
     [SerializeField] private List<Camera> cameras;
-    private int _currentCamNbr;
+    private int _currentCamNbr;*/
+    
+    private Camera _currentCam;
+    public static SwitchCamera Instance;
 
     private void Awake()
     {
-        button.onClick.AddListener(ChangeCameras);
-        _currentCamNbr = 0;
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else Destroy(this);
+        
+        /*button.onClick.AddListener(ChangeCameras);
+        _currentCamNbr = 0;*/
     }
 
-    private void ChangeCameras()
+    /*private void ChangeCameras()
     {
         cameras[_currentCamNbr].gameObject.SetActive(false);
         _currentCamNbr = (_currentCamNbr == cameras.Count - 1) ? 0 : _currentCamNbr + 1;
         cameras[_currentCamNbr].gameObject.SetActive(true);
+    }*/
+
+    public void ChangeCamera(Camera newCamera)
+    {
+        print(_currentCam);
+        print(newCamera);
+        if (_currentCam) _currentCam.gameObject.SetActive(false);
+        _currentCam = newCamera;
+        _currentCam.gameObject.SetActive(true);
     }
 }
