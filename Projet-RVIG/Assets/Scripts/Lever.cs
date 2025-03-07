54af58ac91cdbe5b02d1b4547b5ec7a5fb8d729a
Rapthan <151851356+Rapthan@users.noreply.github.com>
2025-03-07 12:49:11 +0100

    Levier + main + prefab

--- Projet-RVIG/Assets/Scripts/Lever.cs
+++ Projet-RVIG/Assets/Scripts/Lever.cs
@@ -2,11 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Lever : MonoBehaviour
 {
+   private bool _state;
+   
    private void OnTriggerEnter(Collider other)
    {
-      Debug.Log("ça marche !");
+      _state = true;
+   }
+
+   private void OnTriggerExit(Collider other)
+   {
+      _state = false;
+   }
+
+   public bool GetState()
+   {
+      return _state;
    }
 }
