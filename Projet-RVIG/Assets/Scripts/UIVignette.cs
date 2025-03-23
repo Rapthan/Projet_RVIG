1abee699ef6e09b5db7d0764fdde7b70a5367e48
DyablOcOrps <157760105+DyablOcOrps@users.noreply.github.com>
2025-03-23 17:02:36 +0100

    Push du menu de base

--- Projet-RVIG/Assets/Scripts/UIVignette.cs
+++ Projet-RVIG/Assets/Scripts/UIVignette.cs
@@ -8,13 +8,20 @@ public class VignetteController : MonoBehaviour
 {
     [SerializeField] private TunnelingVignetteController vignette;  
     [SerializeField] private Slider vignetteSlider;     // Référence au Slider VR
+    [SerializeField] private bool sliderMove;
 
     void Start()
     {
-        if (vignette)
+        if (vignette && sliderMove)
         {
             // Synchroniser le slider avec la valeur actuelle
-            vignetteSlider.value = vignette.defaultParameters.featheringEffect;
+            vignetteSlider.value = 1-vignette.defaultParameters.apertureSize;
+            vignetteSlider.onValueChanged.AddListener(UpdateVignetteMove);
+        }
+        else 
+        {
+            // Synchroniser le slider avec la valeur actuelle
+            vignetteSlider.value = 1-vignette.defaultParameters.apertureSize;
             vignetteSlider.onValueChanged.AddListener(UpdateVignette);
         }
     }
@@ -22,7 +29,20 @@ public class VignetteController : MonoBehaviour
     // Mise à jour de la vignette avec le slider
     public void UpdateVignette(float value)
     {
-        vignette.defaultParameters.featheringEffect = value;
-        Debug.Log("Vignette intensity: " + value);
+        foreach(var provider in vignette.locomotionVignetteProviders){
+            if (provider != null && provider.locomotionProvider.name.Contains("Turn")){
+                provider.vignetteParameters.apertureSize = 1-value;
+            }
+        }
+    }
+
+    // Mise à jour de la vignette avec le slider
+    public void UpdateVignetteMove(float value)
+    {
+        foreach(var provider in vignette.locomotionVignetteProviders){
+            if (provider != null && provider.locomotionProvider.name.Contains("Move")){
+                provider.vignetteParameters.apertureSize = 1-value;
+            }
+        }
     }
 }
